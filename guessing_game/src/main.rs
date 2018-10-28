5844fad8a5322353455c09e99c223585485ddfee
Sam Smolkin <sam@future-precedent.org>
2018-10-28 11:10:08 +0000

    finished ch 2

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,37 @@
+extern crate rand;
+
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}.", secret_number);
+
+    loop {
+        let mut guess = String::new();
+
+        println!("Enter a number");
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line.");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // println!("You guessed: {}...", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
